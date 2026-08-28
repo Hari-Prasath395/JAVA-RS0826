b70f94c5269c635be4f6d954c4f481b34e26f10d
Hari-Prasath395 <hariprasath395@gmail.com>
2026-08-28 15:12:36 +0530

    updated learnings

--- src/main/java/day21/LocatorsDemo.java
+++ src/main/java/day21/LocatorsDemo.java
@@ -2,6 +2,7 @@ package day21;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -21,11 +22,26 @@ public class LocatorsDemo {
         System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 
         driver.findElement(By.linkText("Forgot your password?")).click();
+
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
         driver.findElement(By.cssSelector(("input[placeholder='Email']"))).sendKeys("john@hra.com");
         driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
+        driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("john@gmail.com");
+        driver.findElement(By.xpath("//form//input[@placeholder='Phone Number']")).sendKeys("123456789");
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        WebElement txtmsg = driver.findElement(By.cssSelector("form p"));
+        System.out.println(txtmsg.getText());
 
-        Thread.sleep(5000);
+        driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']//button[1]")).click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
+        driver.findElement(By.cssSelector(("input[type*='pass']"))).sendKeys("rahulshettyacademy");
+        driver.findElement(By.id("chkboxOne")).click();
+        driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
+
+        Thread.sleep(2000);
         driver.quit();
 
     }
---
+++ src/test/java/JavaPackages/WhileLoopDemo.java
@@ -0,0 +1,32 @@
+package JavaPackages;
+
+public class WhileLoopDemo {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        while(i<=10){
+            System.out.println("Value of i: " + i);
+            i++;
+
+        }
+
+        int j =10;
+        while(j>=0){
+            System.out.println("Value of j: " + j);
+            j--;
+        }
+
+
+
+    //do while loop
+
+    int k = 20;
+    do{
+        System.out.println("Value of k: " + k);
+        k++;
+    }while(k<30);
+}
+
+
+}
